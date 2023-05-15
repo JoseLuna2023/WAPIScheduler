bd87724713dad5e78d1fbedf917165d62072d8ac
JoseLuna2023 <130305124+JoseLuna2023@users.noreply.github.com>
2023-05-15 09:33:29 -0500

    set up script file to check timeBlocks, and add classes linked to css based on time, and to save type in data to local storage, and then replace it on refresh

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var saveButton = $(".saveBtn");
+var currentDay = $("#currentDay")
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+    var date = dayjs().format('MMMM D YYYY');
+    currentDay.text(date);
+    var presentTime = dayjs().hour();
+    var blockId = $(".time-block");
+    blockId.each(function() {
+        var hourValue = $(this).attr("id");
+        if(presentTime < hourValue) {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description future");
+        } else if(presentTime > hourValue) {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description past");
+        } else {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description present");
+        };
+    });
+
+    saveButton.on("click", function(event) {
+        event.preventDefault();
+        var text = $(this).siblings(".description").val().replace(hour);
+        var hour = $(this).parent().attr("id");
+
+        localStorage.setItem(hour, JSON.stringify(text))
+    })
+
+    for(let i = 9; i <= 17; i++) {
+        $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)))
+    }
+});
